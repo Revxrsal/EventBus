a550569c77218c89891815993841491977ab4043
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2021-06-23 22:06:58 +0300

    Add varargs for preGenerate, fix fields not being scanned by annotations other than SubscribeEvent

--- src/main/java/io/github/revxrsal/eventbus/asm/ASMEventBus.java
+++ src/main/java/io/github/revxrsal/eventbus/asm/ASMEventBus.java
@@ -42,7 +42,7 @@ public final class ASMEventBus extends BaseEventBus {
         return super.post(event);
     }
 
-    @Override public void preGenerate(@NotNull Class<?> event) {
-        EventGenerator.generateFactory(event);
+    @Override public void preGenerate(@NotNull Class<?>... event) {
+        for (Class<?> e : event) EventGenerator.generateFactory(e);
     }
 }
