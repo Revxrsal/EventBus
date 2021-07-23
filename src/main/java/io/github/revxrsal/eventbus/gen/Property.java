02e231803b771a9e7342442de75070399405dce0
Reflxction Dev <aloosh.kasasbeh@gmail.com>
2021-07-23 16:43:34 +0300

    Add @Property for defining additional properties in events

--- src/main/java/io/github/revxrsal/eventbus/gen/Property.java
+++ src/main/java/io/github/revxrsal/eventbus/gen/Property.java
@@ -7,8 +7,8 @@ import java.lang.annotation.Target;
 
 /**
  * Represents an additional bean property on the generated event class, which is mutable and
- * is not set using the constructor. The generated property will be
- * non-final, and can have getters and setters.
+ * is not set using the constructor. The generated property will be non-final, null by
+ * default, and can have getters and setters.
  *
  * Getters or setters must follow the getAbc / isAbc / setAbc convention, where 'abc' is the property name
  */
